a5bf3e2e9817dc029eb2547906ce5cbbec4d54bf
vagrant <vagrant@homestead>
2019-02-15 11:52:18 +0000

    changed the layout of subpages

--- resources/views/mainsubsidies.blade.php
+++ resources/views/mainsubsidies.blade.php
@@ -1,20 +1,77 @@
 @extends('layouts.app')
 
 @section('content')
-Subsidies pagina
 
-Omdat wij natuurlijk niet zelf subsidies verstrekken, is de subidiepijler alleen bedoeld om aandacht te geven aan het feit dat de subsidies bestaan. Natuurlijk proberen we wel de ingang en info te geven voordat u in het aanmeldingstraject komt. En we geven de basisinformatie voordat u teleurgesteld ontdekt niet in aanmerking te kunnen komen.
+ 
 
-De belangrijkste regeling voor energie is in Nederland de ISDE.
+    
+    <h1 class="mt-4 mb-3">Subsidie</h1>
 
-Het doel van de regeling Investeringssubsidie Duurzame Energie (ISDE) is het stimuleren van de productie van duurzame energie door middel van met name relatief kleine productie-installaties die geen gebruik kunnen maken van het Besluit stimulering duurzame energie (SDE+).
+    
 
-Geografie: Nationaal
-Subsidieverstrekker: Ministerie van Economische Zaken en Klimaat
-Maximale bijdrage: Onbepaald
-Status: Open voor indiening
-Deadline voor aanvraag: 31-12-2019
+    <div class="row">
 
-<button class="btn btn-info"><a class="text-dark font-weight-bold" href="#">Proceed</a></button>
+      <!-- Subsidies Column -->
+      <div class="col-md-12">
+
+        <!-- Blog Post -->
+        <div class="card mb-4">
+          <img class="card-img-top" src="img/ducks.jpg" alt="Card image cap">
+          <div class="card-body">
+            <h2 class="card-title">Subsidies</h2>
+            <p class="card-text">Omdat wij natuurlijk niet zelf subsidies verstrekken, is de subsidiepijler alleen bedoeld om aandacht te geven aan het feit dat de subsidies bestaan. Natuurlijk proberen we wel de ingang en info te geven voordat u in het aanmeldingstraject komt. En we geven de basisinformatie voordat u teleurgesteld ontdekt niet in aanmerking te kunnen komen.</p>
+			<p>De belangrijkste regeling voor energie is in Nederland de ISDE. Het doel van de regeling Investeringssubsidie Duurzame Energie (ISDE) is het stimuleren van de productie van duurzame energie door middel van met name relatief kleine productie-installaties die geen gebruik kunnen maken van het Besluit stimulering duurzame energie (SDE+).<p>
+			
+      <ul>
+      <li>Geografie: Nationaal</li>
+			<li>Subsidieverstrekker: Ministerie van Economische Zaken en Klimaat</li>
+			<li>Maximale bijdrage: Onbepaald</li>
+			<li>Status: Open voor indiening</li>
+			<li>Deadline voor aanvraag: 31-12-2019.</li>
+      <br>
+            <a href="#" class="btn btn-primary">Lees meer &rarr;</a>
+          
+
+      <div class="col-md-">
+
+        <!-- Categories Widget -->
+        <div class="card my-4">
+          <h5 class="card-header">Linkjes</h5>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-lg-">
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                  <li>
+                    <a href="#">linkje</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+  
+
+
+
+
+
+<!--button class="btn btn-info"><a class="text-dark font-weight-bold" href="#">Proceed</a></button>-->
 
 @endsection
