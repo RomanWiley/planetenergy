e84a1475d240a0647f1f422d456fe77a002bb88d
Alex Coenen <coenena21@gmail.com>
2019-02-15 12:45:51 +0100

    view index products added

---
+++ resources/views/webshopheader.blade.php
@@ -0,0 +1,11 @@
+<ul class="nav nav-pills nav-justified">
+        <li class="nav-item">
+          <a class="nav-link" href="/products">Webshop Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/products/index">Toon producten</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#!">Another Link</a>
+        </li>
+      </ul>
