b7c03b22bbb027cb252d444a1a9eecb47ab65fea
Alex Coenen <coenena21@gmail.com>
2019-02-19 12:01:31 +0100

    updates on navbar webshop

--- resources/views/webshopheader.blade.php
+++ resources/views/webshopheader.blade.php
@@ -1,6 +1,6 @@
 <ul class="nav nav-pills nav-justified">
         <li class="nav-item">
-          <a class="nav-link" href="/product">Webshop Home</a>
+          <a class="nav-link" href="/webshop">Webshop Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#!">Another Link</a>
