9a575a333036f79e0f8d25fecaf90d8bb8ab08f6
Douglas Bowman <d.s.bowman88@gmail.com>
2019-02-21 13:23:19 +0100

    moved webshopsorting.js

---
+++ public/js/webshopsorting.js
@@ -0,0 +1,70 @@
+function sortByName(){
+ list = document.getElementsByClassName("productname"); // get all productnames
+ productdiv = document.getElementsByClassName("productdiv"); // get field for sorting
+ switchcount = 0;
+ switching = true;
+ dir = "asc";
+ while (switching){
+   switching = false
+   for (i = 0; i < (list.length - 1); i++) {
+     shouldSwitch = false;
+     if (dir == "asc") {
+     if (list[i].innerText.toLowerCase() > list[i + 1].innerText.toLowerCase()){
+       shouldSwitch = true;
+       break;
+     }
+   }
+   else if (dir == "desc"){
+     if (list[i].innerText.toLowerCase() < list[i + 1].innerText.toLowerCase()){
+       shouldSwitch = true;
+       break;
+     }
+   }
+ }
+   if (shouldSwitch){
+     productdiv[i].parentNode.insertBefore(productdiv[i + 1], productdiv[i]);
+     switching = true;
+     switchcount ++;
+   } else {
+     if (switchcount == 0 && dir == "asc"){
+       dir = "desc";
+       switching = true;
+   }
+ }
+}
+}
+function sortByPrice(){
+ list = document.getElementsByClassName("productprice"); // get all productprice
+ productdiv = document.getElementsByClassName("productdiv"); // get field for sorting
+ switchcount = 0;
+ switching = true;
+ dir = "asc";
+ while (switching){
+   switching = false
+   for (i = 0; i < (list.length - 1); i++) {
+     shouldSwitch = false;
+     if (dir == "asc") {
+     if (list[i].innerText.toLowerCase() > list[i + 1].innerText.toLowerCase()){
+       shouldSwitch = true;
+       break;
+     }
+   }
+   else if (dir == "desc"){
+     if (list[i].innerText.toLowerCase() < list[i + 1].innerText.toLowerCase()){
+       shouldSwitch = true;
+       break;
+     }
+   }
+ }
+   if (shouldSwitch){
+     productdiv[i].parentNode.insertBefore(productdiv[i + 1], productdiv[i]);
+     switching = true;
+     switchcount ++;
+   } else {
+     if (switchcount == 0 && dir == "asc"){
+       dir = "desc";
+       switching = true;
+   }
+ }
+}
+}
