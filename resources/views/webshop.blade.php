6dd7174af857bf2eb9526c90bce8f90f0db8064d
Douglas Bowman <d.s.bowman88@gmail.com>
2019-02-26 15:27:27 +0100

    made cards for products

--- resources/views/webshop.blade.php
+++ resources/views/webshop.blade.php
@@ -22,31 +22,30 @@ $products=App\Product::all()
 
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 @include('productheader')
                 <br>
                 <br>
-                 <div id="filterme" class="panel-body">
+                 <div id="filterme" class="panel-body flex-container d-flex flex-wrap useflex">
                     @foreach($products as $product)
                     <?php
+
                     $link = $product->getFirstMediaUrl('product_images');
                     $old = "localhost";
                     $new = "pe.test";
                     $newlink = str_replace($old, $new, $link);
                     ?>
-                        <div class="filterdiv">
+                        <div class="filterdiv card col-md-3 m-2">
                         <a href='/product/{{$product->id}}' style="text-decoration:none;color:black">
-                            <article class="productdiv">
-                                <H4 class="productname"><b>{{ $product->product_name }}</b></H4>
+                            <article class="productdiv card-body">
+                                <H4 class="productname card-title"><b>{{ $product->product_name }}</b></H4>
                                 <img src="<?= $newlink?>" alt="No image found">
-                                <div class='body'>
-                                    Categorie: {{ $product->product_category }}<BR>&#8364
+                                <div class='body card-text'>
+                                    Categorie: {{ $product->product_category }}<BR> &#8364
                                     <span class="productprice"> {{ $product->price }} </span>
                             </div>
                         </a>
-
-                            <hr>
                             </article>
                         </div>
                     @endforeach
