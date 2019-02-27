4a7ca14ceaf9d4d207c4f659eff4300820dae7c7
Martijn Blokhuis <blokhuis.m@gmail.com>
2019-02-27 14:43:41 +0100

    added more input fields

--- resources/views/aanvraagsubsidie.blade.php
+++ resources/views/aanvraagsubsidie.blade.php
@@ -8,11 +8,31 @@
 </div>
 
 <form action="aanvraagbevestiging" method="get" target="_blank">
+<label class="label" for="product_category">Categorie</label>
+    <select id="select_category" name="product_category" onchange="filteroncategory()">
+        <option value="Zon">Zon</option>
+        <option value="Wind">Wind</option>
+        <option value="WBSO">Wet Bevordering Speur- en Ontwikkelingswerk</option>
+    </select>
+    <BR>
 <input type="text"id="asnaam" name="naam" placeholder="Naam...">
+<BR>
+<input type="text"id="asadres" name="adres" placeholder="Adres...">
+<BR>
+<input type="text"id="aswoonplaats" name="woonplaats" placeholder="Woonplaats...">
+<BR>
+<input type="text"id="aspostcode" name="postcode" placeholder="Postcode...">
+<BR>
+<input type="text"id="astelefoon" name="telefoonnummer" placeholder="Telefoon...">
+<BR>
+<input type="text"id="asnaam" name="email" placeholder="Email...">
+<BR>
+<textarea rows="4" cols="50" name="comment" form="usrform" placeholder="Opmerkingen...">
+</textarea>
+
+<BR>
 <input type="submit" value="Submit">
 
-
 </form>
-
 
 @endsection
