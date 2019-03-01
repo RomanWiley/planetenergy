5b093469efeed48cbf21619bfc344cb0febbb045
Douglas Bowman <d.s.bowman88@gmail.com>
2019-03-01 16:22:06 +0100

    made code a bit more readable

--- resources/views/aanvraagbevestiging.blade.php
+++ resources/views/aanvraagbevestiging.blade.php
@@ -3,39 +3,46 @@
 <br>
 
 <div class="container" style="text-align: center; ">
-    <h2>Bevestiging subsidie aanvraag</h2><BR>
+    <h2>Bevestiging subsidie aanvraag</h2><br>
 </div>
 <?php
 $input = Request::all();
 ?>
 <div style="border-style: solid; border-radius: 25px; background-color: lightgray; padding: 25px;" >
 <b>Subsidie Categorie: </b>
-{{$input['subsidie_category']}}
-<BR>
+    {{$input['subsidie_category']}}
+<br>
+
 <b>Naam: </b>
-{{$input['naam']}}
- <br>
- <b>Adres: </b>
- {{$input['adres']}}
- <br>
- <b>Woonplaats: </b>
- {{$input['woonplaats']}}
- <br>
- <b>Postcode: </b>
- {{$input['postcode']}}
- <BR>
- <b>Telefoon: </b>
- {{$input['telefoonnummer']}}
- <BR>
- <b>Email: </b>
- {{$input['email']}}
- <BR>
- <b>Opmerkingen: </b>
- {{$input['comment']}}
-<BR>
+    {{$input['naam']}}
+<br>
+
+<b>Adres: </b>
+    {{$input['adres']}}
+<br>
+
+<b>Woonplaats: </b>
+    {{$input['woonplaats']}}
+<br>
+
+<b>Postcode: </b>
+    {{$input['postcode']}}
+<br>
+
+<b>Telefoon: </b>
+    {{$input['telefoonnummer']}}
+<br>
+
+<b>Email: </b>
+    {{$input['email']}}
+<br>
+
+<b>Opmerkingen: </b>
+    {{$input['comment']}}
+<br>
 
 </div>
-<BR>
+<br>
 <a href="/aanvraagsubsidie" class="btn btn-danger" >Terug</a>
 <button type="submit" class="btn btn-primary" id="sendMessageButton">Verzend</button>
 
