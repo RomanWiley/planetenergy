c052e9148c47d114bae1fbf439525cd21ccea55f
Martijn Blokhuis <blokhuis.m@gmail.com>
2019-03-01 13:31:32 +0100

    some changes in the subsidie form

--- resources/views/aanvraagbevestiging.blade.php
+++ resources/views/aanvraagbevestiging.blade.php
@@ -5,6 +5,8 @@
 <div class="container">
     Bevestiging subsidie aanvraag
 </div>
+
+
 <BR>
 <button type="submit" class="btn btn-primary" id="sendMessageButton">Terug</button>
 <button type="submit" class="btn btn-primary" id="sendMessageButton">Verzend</button>
