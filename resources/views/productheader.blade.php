0e74946c9cf9bf0262a72ff87e13149c376df78f
Douglas Bowman <d.s.bowman88@gmail.com>
2019-03-07 13:55:37 +0100

    edited button text styling

--- resources/views/productheader.blade.php
+++ resources/views/productheader.blade.php
@@ -3,8 +3,8 @@
 
 @auth
 @if (Auth::user()->isCompany == 1)
-<a class="text-dark font-weight-bold" href="/product/create"><button class="btn btn-info">Nieuw product</button></a>
-<a class="text-dark font-weight-bold" href="/product"><button class="btn btn-success">Mijn Producten</button></a>
+<a class="text-dark font-weight-bold" href="/product/create"><button class="btn btn-info text-dark"><b>Nieuw product</b></button></a>
+<a class="text-dark font-weight-bold" href="/product"><button class="btn btn-success text-dark"><b>Mijn Producten</b></button></a>
 @endif
 @endauth
 
