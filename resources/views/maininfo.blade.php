802ff60584aaf3436e5f0910b4be28e2fff9b021
Alex Coenen <coenena21@gmail.com>
2019-03-08 10:41:15 +0100

    maininfo page, text added

--- resources/views/maininfo.blade.php
+++ resources/views/maininfo.blade.php
@@ -2,7 +2,20 @@
 
 @section('content')
 <br>
-Hier komt de algemene tekst over planet energy
-
+<h1>Wat is Planet Energy?</h1>
+<br>
+<div class="container" style="font-size: 150%">
+Planet Energy is een niet-commercieel platform voor bedrijven en particulieren.
+Duurzaamheid en groene energie is het thema van het platform, met een sociale achtergrond.
+Voor de bedrijven zou het een kans moeten bieden producten meer bekendheid te geven, alsmede de bijbehorende innovaties.
+Voor wat betreft het sociale deel;
+mikro-kredieten of aantrekkelijke regelingen en subsidies voor het uitvoeren/kopen van duurzame innovaties en producten.
+<br><br>
+Het idee erachter is dat iedereen de nieuwste ontwikkelingen en producten kan benaderen zonder dagenlange zoektochten.
+<br><br>
+Hiervoor bieden wij een webshop, forum, subsidie-info en -aanvragen en lening-info en -aanvragen.
+Bedrijven en instanties kunnen zich ook verzamelen binnen onze site onder de link-page.
+<br>
+</div>
 
 @endsection
